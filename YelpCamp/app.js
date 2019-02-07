064bc6a863df92cfe94a02c4d36d710d275477af
Goce Ilievski <goce.ilievski@live.com>
2019-02-07 18:02:48 +0000

    mongolab connect

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -20,7 +20,8 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp");
+// mongoose.connect("mongodb://localhost:27017/yelp_camp");
+mongoose.connect("mongodb://Gotse:superpassword123@ds125945.mlab.com:25945/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
